44e7cb56e613549041ad39ffe8c9a9214594c38a
Michał Jagiełło <jagiello.m@gmail.com>
2020-04-03 21:33:33 +0200

    Added migration

---
+++ UszyjMaseczke.Migrations/Migrations/20200403191238_AddedAdditionalComment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UszyjMaseczke.Migrations.Migrations
+{
+    public partial class AddedAdditionalComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalComment",
+                table: "Requests",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdditionalComment",
+                table: "Requests");
+        }
+    }
+}
